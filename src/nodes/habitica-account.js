a6ed28f621c0427aabbe4e98628d1829547ae804
Bill Parrott <bill@chimericdream.com>
2020-11-25 01:17:10 -0600

    feat: need more than just username and api token; user id too [feature/3-config-nodes]

--- src/nodes/habitica-account.js
+++ src/nodes/habitica-account.js
@@ -3,7 +3,8 @@ module.exports = RED => {
         RED.nodes.createNode(this, n);
 
         this.username = n.username;
-        this.apiKey = n.apiKey;
+        this.userId = n.userId;
+        this.apiToken = n.apiToken;
     }
 
     RED.nodes.registerType('habitica-account', HabiticaAccountNode);
