0c8715d93f055106c2a962c062961212b4b75822
Bill Parrott <bill@chimericdream.com>
2020-11-26 11:43:56 -0600

    feat: basic profile fetching works [feature/5-fetch-user-data]

--- src/nodes/user-profile.js
+++ src/nodes/user-profile.js
@@ -1,13 +1,47 @@
+const https = require('https');
+
 module.exports = RED => {
     function UserProfileNode(config) {
         const node = this;
 
         RED.nodes.createNode(node, config);
 
-        node.on('input', async function(msg) {
-            msg.payload = JSON.stringify(node, null, 4);
+        node.apiAccount = RED.nodes.getNode(config.apiAccount);
+        node.account = RED.nodes.getNode(config.account);
 
-            node.send(msg);
+        node.on('input', async function(msg) {
+            const opts = {
+                host: 'habitica.com',
+                path: '/api/v3/user',
+                headers: {
+                    'X-Client': `${node.apiAccount.userId}-NodeRED`,
+                    'X-API-User': node.account.userId,
+                    'X-API-Key': node.account.apiToken,
+                },
+            };
+
+            https.get(opts, req => {
+                req.setEncoding('utf-8');
+
+                let body = '',
+                    response = null;
+
+                req.on('data', data => {
+                    body += data;
+                });
+
+                req.on('end', () => {
+                    response = JSON.parse(body);
+
+                    msg.payload = response;
+                    node.send(msg);
+                    node.status({fill: 'green', shape: 'dot', text: 'Profile retrieved'});
+                });
+            }).on('error', err => {
+                console.log(err);
+                node.error('Something done broke');
+                node.status({fill: 'red', shape: 'ring', text: 'Error retrieving profile'});
+            });
         });
     }
 
